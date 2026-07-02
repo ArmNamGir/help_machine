7fdff3c0702f548c9df71a84c811922cc6759b94
ArmNamGir <armand.houpin@namma-france.com>
2026-07-02 11:08:52 +0200

    Add files via upload

---
+++ group_user.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# ===================== CONFIG =====================
+sudo groupadd namma-dsf
+sudo usermod -aG namma-dsf namma
+sudo usermod -aG namma-dsf dsf
+sudo chown -R dsf:namma-dsf /opt/dsf/sd
+sudo find /opt/dsf/sd -type d -exec chmod 2775 {} \;
+sudo find /opt/dsf/sd -type f -exec chmod 664 {} \;
+sudo setfacl -R -m u:namma:rwx,u:dsf:rwx /opt/dsf/sd
+sudo setfacl -R -d -m u:namma:rwx,u:dsf:rwx /opt/dsf/sd
